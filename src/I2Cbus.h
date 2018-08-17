8681bc6effa8d97d75aec2e9b7714340b75f5c52
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2018-08-17 14:05:38 +0200

    typos

--- src/I2Cbus.h
+++ src/I2Cbus.h
@@ -6,16 +6,16 @@
 #define __I2CBUS_H_
  
  
-
 // This routine implements an I2C bus clear on lines `sda` and `scl`.
 // On return the pins `sda` and `scl` need to be reassigned for I2C,
-// so call Wire.begin() after I2Cbus_clear.
+// so call Wire.begin() after I2Cbus_clear().
 //
 // This function returns the realized bus status:
 // positive numbers indicate success, negative numbers failure.
 // Use I2Cbus_statusstr() to convert status to printable string.
 int I2Cbus_clear(int sda, int scl);
 
+// Convert status returned by I2Cbus_clear() into a printable string.
 const char * I2Cbus_statusstr(int status);
 
 #endif
